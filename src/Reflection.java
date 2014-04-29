d40cdaf7e7f9c40e702c0396a7d52e13dbf5b3d4
MacSzh <szhcjh@gmail.com>
2014-04-29 15:04:13 +0800

    test

---
+++ src/Reflection.java
@@ -0,0 +1,39 @@
+import util.Log;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Type;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Created by shenzaih on 2014/4/25.
+ */
+
+enum Explore{HERE,THERE}
+
+public class Reflection {
+    public static Set<String> analyze(Class<?> enumClass){
+        Log.d("-----Analyzing " + enumClass + "------");
+        Log.d("Interfaces:");
+        for (Type type: enumClass.getGenericInterfaces()){
+            Log.d(type);
+        }
+        Log.d("Base: " + enumClass.getGenericInterfaces() );
+        Log.d("Methods: ");
+        Set<String> methods = new TreeSet<String>();
+        for (Method method: enumClass.getMethods())
+            methods.add(method.getName());
+        Log.d(methods);
+        return methods;
+    }
+
+    public static void main(String[] args) {
+        Set<String> exploreMethods = analyze(Explore.class) ;
+        Set<String> enumMethods = analyze(Enum.class);
+        Log.d("Explore.containsAll(enumMethods) ?" + exploreMethods.containsAll(enumMethods));
+        Log.d("exploreMethods.removeAll(enumMethods)");
+        exploreMethods.removeAll(enumMethods);
+        Log.d(exploreMethods);
+
+    }
+}
